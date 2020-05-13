97aa92ade3582a4b32c263cfae89420338bb10ca
Thomas Versteeg <thomasversteeg@gmx.com>
2020-05-13 22:44:27 +0200

    WIP - connect physics to render

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,24 @@ mod physics;
 mod render;
 
 use crate::{
-    physics::Physics,
+    physics::{Physics, RigidBody},
     render::{Instance, Render},
 };
 use anyhow::Result;
-use legion::world::{Universe, World};
+use legion::{
+    query::{IntoQuery, Read, Write},
+    schedule::Schedule,
+    system::SystemBuilder,
+    world::{Universe, World},
+};
 use miniquad::{
     conf::{Conf, Loading},
     Context, EventHandler, UserData,
 };
+use ncollide2d::shape::Ball;
 
 type Vec2 = nalgebra::Vector2<f64>;
+type Velocity = nphysics2d::math::Velocity<f64>;
 
 const WIDTH: usize = 800;
 const HEIGHT: usize = 600;
@@ -27,6 +34,8 @@ struct Game {
     universe: Universe,
     /// ECS world.
     world: World,
+    /// ECS schedule for the systems.
+    schedule: Schedule,
 }
 
 impl Game {
@@ -39,30 +48,59 @@ impl Game {
         let character_mesh = render.upload_svg(include_str!("../assets/single-character.svg"))?;
 
         // Instantiate the physics engine
-        let physics = Physics::new(-9.81);
+        let mut physics = Physics::new(-9.81);
 
         // Instantiate the ECS
         let universe = Universe::new();
         let mut world = universe.create_world();
 
-        // Add 10 characters
+        // Add 10 characters with rigid bodies
         world.insert(
             (character_mesh,),
-            (0..9).map(|x| (Instance::new(x as f32 * 100.0, 0.0),)),
+            (0..9).map(|x| {
+                let rigid_body_desc = Physics::default_rigid_body_builder(
+                    Vec2::new(x as f64, 0.0),
+                    Velocity::linear(0.0, 0.0),
+                );
+                let collider_body_desc = Physics::default_collider_builder(Ball::new(1.5));
+                (
+                    Instance::new(x as f32 * 100.0, 0.0),
+                    physics.spawn_rigid_body(&rigid_body_desc, &collider_body_desc),
+                )
+            }),
         );
+
+        // Create the system for updating the instance positions
+        let update_positions = SystemBuilder::<()>::new("update_positions")
+            .with_query(<(Write<Instance>, Read<RigidBody>)>::query())
+            .build(|_, mut world, _, query| {
+                for (mut instance, rigid_body) in query.iter_mut(&mut world) {
+                    instance.x += 1.0;
+                }
+            });
+
+        let mut schedule = Schedule::builder()
+            .add_system(update_positions)
+            .flush()
+            .build();
 
         Ok(Self {
             render,
             physics,
             universe,
             world,
+            schedule,
         })
     }
 }
 
 impl EventHandler for Game {
     fn update(&mut self, _ctx: &mut Context) {
+        // Move the physics
         self.physics.step();
+
+        // Run the systems scheduler
+        self.schedule.execute(&mut self.world);
 
         self.render.update(&mut self.world);
     }
